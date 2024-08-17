751be133550266b406237761a98348de17b78d35
Alireza <alireza.kariminejad@yahoo.com>
2024-08-17 06:47:05 +0330

    add banner

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_shop_application/data/data_source/authentication_data_source.dart';
+import 'package:flutter_shop_application/data/data_source/banner_data_source.dart';
 import 'package:flutter_shop_application/data/data_source/category_data_source.dart';
 import 'package:flutter_shop_application/data/repository/authentication_repository.dart';
+import 'package:flutter_shop_application/data/repository/banner_repository.dart';
 import 'package:flutter_shop_application/data/repository/category_repository.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -12,23 +14,20 @@ var locator = GetIt.instance;
 
 Future<void> getItInit() async {
   // components
-  locator.registerSingleton<Dio>(
-      Dio(BaseOptions(baseUrl: 'https://startflutter.ir/api/')));
+  locator.registerSingleton<Dio>(Dio(BaseOptions(baseUrl: 'https://startflutter.ir/api/')));
 
-  locator.registerSingleton<SharedPreferences>(
-      await SharedPreferences.getInstance());
+  locator.registerSingleton<SharedPreferences>(await SharedPreferences.getInstance());
 
 //datasources
-  locator
-      .registerFactory<IAuthenticationDatasource>(() => RemoteAuthentication());
-  locator
-      .registerFactory<ICategoryDataSource>(() => CategoryRemoteDataSource());
+  locator.registerFactory<IAuthenticationDatasource>(() => RemoteAuthentication());
+  locator.registerFactory<ICategoryDataSource>(() => CategoryRemoteDataSource());
+  locator.registerFactory<IBannerDataSource>(() => BannerRemoteDataSource());
 
   //repositories
-  locator.registerFactory<IAuthenticationRepository>(
-      () => AuthenticationRepository());
+  locator.registerFactory<IAuthenticationRepository>(() => AuthenticationRepository());
 
   locator.registerFactory<ICateogoryRepository>(() => CategoryRepository());
+  locator.registerFactory<IBannerRepository>(() => BannerRepository());
 }
 
 // var locator = GetIt.instance;
