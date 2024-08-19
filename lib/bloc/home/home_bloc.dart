d567ffd401a8cf3e694071e2b1174008e85e158d
Alireza <alireza.kariminejad@yahoo.com>
2024-08-19 19:28:28 +0330

    add home bloc

---
+++ lib/bloc/home/home_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_shop_application/bloc/home/home_event.dart';
+import 'package:flutter_shop_application/bloc/home/home_state.dart';
+import 'package:flutter_shop_application/data/repository/banner_repository.dart';
+import 'package:flutter_shop_application/di/di.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  final IBannerRepository _bannerRepository = locator.get();
+  HomeBloc() : super(HomeInitialState()) {
+    on<HomeGetInitalizedDataEvent>(
+      (event, emit) async {
+        emit(HomeLoadingState());
+        var bannerList = await _bannerRepository.getBanners();
+        emit(HomeRequestSuccessState(bannerList));
+      },
+    );
+  }
+}
