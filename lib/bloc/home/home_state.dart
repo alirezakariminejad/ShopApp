47003341d54282b0dd32b4c39ae75bde885f15a2
Alireza <alireza.kariminejad@yahoo.com>
2024-08-22 16:53:47 +0330

    add category to home state

--- lib/bloc/home/home_state.dart
+++ lib/bloc/home/home_state.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_shop_application/model/banner_model.dart';
+import 'package:flutter_shop_application/model/category.dart';
 
 abstract class HomeState {}
 
@@ -9,5 +10,6 @@ class HomeInitialState extends HomeState {}
 
 class HomeRequestSuccessState extends HomeState {
   Either<String, List<BannerModel>> bannerList;
-  HomeRequestSuccessState(this.bannerList);
+  Either<String, List<Category>> categoryList;
+  HomeRequestSuccessState(this.bannerList, this.categoryList);
 }
