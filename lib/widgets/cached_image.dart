bf5544143bdf72afb98a2914d32685fc8f07ce7e
Alireza <alireza.kariminejad@yahoo.com>
2024-08-22 16:56:03 +0330

    add fit and height

--- lib/widgets/cached_image.dart
+++ lib/widgets/cached_image.dart
@@ -3,14 +3,18 @@ import 'package:flutter/material.dart';
 
 class CachedImage extends StatelessWidget {
   String? imageUrl;
-   CachedImage({super.key, this.imageUrl});
+  BoxFit fit;
+  double? imageHeight;
+   CachedImage({super.key, this.imageUrl, this.fit=BoxFit.cover, this.imageHeight});
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
+      borderRadius: BorderRadius.circular(15.0),
       child: CachedNetworkImage(
         imageUrl: imageUrl ?? '',
-        fit: BoxFit.cover,
+        fit: fit,
+        height: imageHeight,
         errorWidget: (context, url, error) => Container(color: Colors.red[500]),
         placeholder: (context, url) => Container(color: Colors.grey),
       ),
