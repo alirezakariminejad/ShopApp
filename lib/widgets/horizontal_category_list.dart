ed5cae94faea89033b3cb5a60c1f9c6752c7e00d
Alireza <alireza.kariminejad@yahoo.com>
2024-08-22 16:56:44 +0330

    change to get data

--- lib/widgets/horizontal_category_list.dart
+++ lib/widgets/horizontal_category_list.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_application/model/category.dart';
+import 'package:flutter_shop_application/widgets/cached_image.dart';
 
 class HorizontalCategoryList extends StatelessWidget {
   final double? leftPadding;
-  const HorizontalCategoryList({super.key, this.leftPadding = 20.0});
+  final Category category;
+  HorizontalCategoryList(this.category, {super.key, this.leftPadding = 20.0});
 
   @override
   Widget build(BuildContext context) {
+    String categoryColor = 'FF${category.color}';
+    int hexColor = int.parse(categoryColor, radix: 16);
+    int redColor = Color(hexColor).red;
+    int greenColor = Color(hexColor).green;
+    int blueColor = Color(hexColor).blue;
+
     return Padding(
       padding: EdgeInsets.only(left: leftPadding!),
       child: Column(
@@ -14,27 +23,28 @@ class HorizontalCategoryList extends StatelessWidget {
             width: 56.0,
             height: 56.0,
             decoration: ShapeDecoration(
-              color: const Color(0XFFFBAD40),
+              color: Color(hexColor),
               shape: ContinuousRectangleBorder(
                 borderRadius: BorderRadius.circular(45.0),
               ),
-              shadows: const [
+              shadows: [
                 BoxShadow(
-                  color: Color.fromRGBO(251, 173, 64, 0.75),
+                  color: Color.fromRGBO(redColor, greenColor, blueColor, 0.75),
                   blurRadius: 30.0,
                   spreadRadius: -5.0,
                   offset: Offset(0, 10.0),
                 ),
               ],
             ),
-            child: Image.asset(
-              'assets/images/iphone_icon.png',
-              height: 26.0,
+            child: CachedImage(
+              imageUrl: category.icon,
+              fit: BoxFit.none,
+              imageHeight: 24.0,
             ),
           ),
           const SizedBox(height: 10.0),
-          const Text(
-            'آیفون',
+          Text(
+            '${category.title}',
             style: TextStyle(
               color: Colors.black,
               fontWeight: FontWeight.bold,
