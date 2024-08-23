d5384678ec96285483418bd27a0beaf0e976502c
Alireza <alireza.kariminejad@yahoo.com>
2024-08-23 14:35:47 +0330

    add registerFactory for product

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -4,9 +4,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter_shop_application/data/data_source/authentication_data_source.dart';
 import 'package:flutter_shop_application/data/data_source/banner_data_source.dart';
 import 'package:flutter_shop_application/data/data_source/category_data_source.dart';
+import 'package:flutter_shop_application/data/data_source/product_data_source.dart';
 import 'package:flutter_shop_application/data/repository/authentication_repository.dart';
 import 'package:flutter_shop_application/data/repository/banner_repository.dart';
 import 'package:flutter_shop_application/data/repository/category_repository.dart';
+import 'package:flutter_shop_application/data/repository/product_repository.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -22,28 +24,11 @@ Future<void> getItInit() async {
   locator.registerFactory<IAuthenticationDatasource>(() => RemoteAuthentication());
   locator.registerFactory<ICategoryDataSource>(() => CategoryRemoteDataSource());
   locator.registerFactory<IBannerDataSource>(() => BannerRemoteDataSource());
+  locator.registerSingleton<IProductDataSource>(ProductRemoteDataSource());
 
   //repositories
   locator.registerFactory<IAuthenticationRepository>(() => AuthenticationRepository());
-
   locator.registerFactory<ICateogoryRepository>(() => CategoryRepository());
   locator.registerFactory<IBannerRepository>(() => BannerRepository());
+  locator.registerFactory<IProducRepository>(() => ProductRepository());
 }
-
-// var locator = GetIt.instance;
-// Future<void> getItInit() async {
-//   // component
-//   locator.registerSingleton<Dio>(
-//       Dio(BaseOptions(baseUrl: 'https://startflutter.ir/api/')));
-
-//   locator.registerSingleton<SharedPreferences>(
-//       await SharedPreferences.getInstance());
-
-//   //datasources
-//   locator
-//       .registerFactory<IAuthenticationDatasource>(() => RemoteAuthentication());
-
-//   //repositories
-//   locator.registerFactory<IAuthenticationRepository>(
-//       () => AuthenticationRepository());
-// }
