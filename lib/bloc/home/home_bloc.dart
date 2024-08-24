4ae0a383b38d37549ea80bcab71ca06699d32172
Alireza <alireza.kariminejad@yahoo.com>
2024-08-24 18:46:07 +0330

    add best seller and hottest

--- lib/bloc/home/home_bloc.dart
+++ lib/bloc/home/home_bloc.dart
@@ -3,18 +3,29 @@ import 'package:flutter_shop_application/bloc/home/home_event.dart';
 import 'package:flutter_shop_application/bloc/home/home_state.dart';
 import 'package:flutter_shop_application/data/repository/banner_repository.dart';
 import 'package:flutter_shop_application/data/repository/category_repository.dart';
+import 'package:flutter_shop_application/data/repository/product_repository.dart';
 import 'package:flutter_shop_application/di/di.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final IBannerRepository _bannerRepository = locator.get();
   final ICateogoryRepository _categoryRepository = locator.get();
+  final IProducRepository _productRepository = locator.get();
+
   HomeBloc() : super(HomeInitialState()) {
     on<HomeGetInitalizedDataEvent>(
       (event, emit) async {
         emit(HomeLoadingState());
+        
         var bannerList = await _bannerRepository.getBanners();
         var categoryList = await _categoryRepository.getRepositories();
-        emit(HomeRequestSuccessState(bannerList, categoryList));
+        var productList = await _productRepository.getProductsRepositories();
+        emit(HomeRequestSuccessState(bannerList, categoryList, productList));
+
+        var bestSellerProduct = await _productRepository.getBestSellerProductsRepositories();
+        emit(HomeRquestBestSellerProductState(bestSellerProduct));
+
+        var hottestProduct = await _productRepository.getHottestProductsRepositories();
+        emit(HomeRquestHottestProductState(hottestProduct));
       },
     );
   }
