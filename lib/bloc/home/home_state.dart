0bad91418d84b0c2456a2bb7ce097fcb0c404a2c
Alireza <alireza.kariminejad@yahoo.com>
2024-08-24 18:46:39 +0330

    add best seller and hottest

--- lib/bloc/home/home_state.dart
+++ lib/bloc/home/home_state.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_shop_application/model/banner_model.dart';
 import 'package:flutter_shop_application/model/category.dart';
+import 'package:flutter_shop_application/model/product.dart';
 
 abstract class HomeState {}
 
@@ -11,5 +12,16 @@ class HomeInitialState extends HomeState {}
 class HomeRequestSuccessState extends HomeState {
   Either<String, List<BannerModel>> bannerList;
   Either<String, List<Category>> categoryList;
-  HomeRequestSuccessState(this.bannerList, this.categoryList);
+  Either<String, List<Product>> productList;
+  HomeRequestSuccessState(this.bannerList, this.categoryList, this.productList);
+}
+
+class HomeRquestBestSellerProductState extends HomeState {
+  Either<String, List<Product>> bestSellerProductList;
+  HomeRquestBestSellerProductState(this.bestSellerProductList);
+}
+
+class HomeRquestHottestProductState extends HomeState {
+    Either<String, List<Product>> hottestProductList;
+  HomeRquestHottestProductState(this.hottestProductList);
 }
