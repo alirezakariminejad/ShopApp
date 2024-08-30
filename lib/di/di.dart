64081c92aa433bb16b46b84ad4d338cd6de1879f
Alireza <alireza.kariminejad@yahoo.com>
2024-08-30 11:07:52 +0330

    add ProductDetails

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -5,9 +5,11 @@ import 'package:flutter_shop_application/data/data_source/authentication_data_so
 import 'package:flutter_shop_application/data/data_source/banner_data_source.dart';
 import 'package:flutter_shop_application/data/data_source/category_data_source.dart';
 import 'package:flutter_shop_application/data/data_source/product_data_source.dart';
+import 'package:flutter_shop_application/data/data_source/product_details_datasource.dart';
 import 'package:flutter_shop_application/data/repository/authentication_repository.dart';
 import 'package:flutter_shop_application/data/repository/banner_repository.dart';
 import 'package:flutter_shop_application/data/repository/category_repository.dart';
+import 'package:flutter_shop_application/data/repository/product_details_repository.dart';
 import 'package:flutter_shop_application/data/repository/product_repository.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -25,10 +27,12 @@ Future<void> getItInit() async {
   locator.registerFactory<ICategoryDataSource>(() => CategoryRemoteDataSource());
   locator.registerFactory<IBannerDataSource>(() => BannerRemoteDataSource());
   locator.registerSingleton<IProductDataSource>(ProductRemoteDataSource());
+  locator.registerSingleton<IProductDetailsDataSource>(ProductDetailsRemoteDatasource());
 
   //repositories
   locator.registerFactory<IAuthenticationRepository>(() => AuthenticationRepository());
   locator.registerFactory<ICateogoryRepository>(() => CategoryRepository());
   locator.registerFactory<IBannerRepository>(() => BannerRepository());
   locator.registerFactory<IProducRepository>(() => ProductRepository());
+  locator.registerSingleton<IProductDetailsRepository>(ProductDetailsRepository());
 }
