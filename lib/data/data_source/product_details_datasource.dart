9404a87a0ff1ad3b6e43f6c5f9aef0d35511398f
Alireza <alireza.kariminejad@yahoo.com>
2024-09-02 11:52:26 +0330

    add getVariantTypes

--- lib/data/data_source/product_details_datasource.dart
+++ lib/data/data_source/product_details_datasource.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_shop_application/di/di.dart';
 import 'package:flutter_shop_application/model/product_images.dart';
+import 'package:flutter_shop_application/model/variant_type.dart';
 import 'package:flutter_shop_application/utils/api_exception.dart';
 
 abstract class IProductDetailsDataSource {
   Future<List<ProductImages>> getGallery();
+  Future<List<VariantType>> getVariantTypes();
 }
 
 class ProductDetailsRemoteDatasource extends IProductDetailsDataSource {
@@ -19,6 +21,20 @@ class ProductDetailsRemoteDatasource extends IProductDetailsDataSource {
       );
       return response.data['items']
           .map<ProductImages>((jsonObject) => ProductImages.fromMapJson(jsonObject))
+          .toList();
+    } on DioError catch (ex) {
+      throw ApiException(ex.response?.statusCode, ex.response?.data['message']);
+    } catch (ex) {
+      throw ApiException(0, 'unknown error');
+    }
+  }
+
+  @override
+  Future<List<VariantType>> getVariantTypes() async {
+    try {
+      var response = await _dio.get('collections/variants_type/records');
+      return response.data['items']
+          .map<VariantType>((jsonObject) => VariantType.fromMapJson(jsonObject))
           .toList();
     } on DioError catch (ex) {
       throw ApiException(ex.response?.statusCode, ex.response?.data['message']);
