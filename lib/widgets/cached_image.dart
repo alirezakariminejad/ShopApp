c4b4a7dffc97a94f36973045ddc173cf943b4227
Alireza <alireza.kariminejad@yahoo.com>
2024-09-02 11:20:35 +0330

    add radius

--- lib/widgets/cached_image.dart
+++ lib/widgets/cached_image.dart
@@ -5,12 +5,19 @@ class CachedImage extends StatelessWidget {
   String? imageUrl;
   BoxFit fit;
   double? imageHeight;
-   CachedImage({super.key, this.imageUrl, this.fit=BoxFit.cover, this.imageHeight});
+  double radius;
+  CachedImage({
+    super.key,
+    this.imageUrl,
+    this.fit = BoxFit.cover,
+    this.imageHeight,
+    this.radius = 15.0,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(15.0),
+      borderRadius: BorderRadius.circular(radius),
       child: CachedNetworkImage(
         imageUrl: imageUrl ?? '',
         fit: fit,
@@ -18,7 +25,6 @@ class CachedImage extends StatelessWidget {
         errorWidget: (context, url, error) => Container(color: Colors.red[500]),
         placeholder: (context, url) => Container(color: Colors.grey),
       ),
-    ); 
+    );
   }
 }
- 
