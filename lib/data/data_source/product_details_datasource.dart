f329c620481592e313d504de3b09d0cd58865c82
Alireza <alireza.kariminejad@yahoo.com>
2024-09-06 11:06:10 +0330

    add getVariants() and getProductVariants()

--- lib/data/data_source/product_details_datasource.dart
+++ lib/data/data_source/product_details_datasource.dart
@@ -1,12 +1,16 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_shop_application/di/di.dart';
 import 'package:flutter_shop_application/model/product_images.dart';
+import 'package:flutter_shop_application/model/product_variant.dart';
+import 'package:flutter_shop_application/model/variant.dart';
 import 'package:flutter_shop_application/model/variant_type.dart';
 import 'package:flutter_shop_application/utils/api_exception.dart';
 
 abstract class IProductDetailsDataSource {
   Future<List<ProductImages>> getGallery();
   Future<List<VariantType>> getVariantTypes();
+  Future<List<Variant>> getVariants();
+  Future<List<ProductVariant>> getProductVariants();
 }
 
 class ProductDetailsRemoteDatasource extends IProductDetailsDataSource {
@@ -14,7 +18,7 @@ class ProductDetailsRemoteDatasource extends IProductDetailsDataSource {
   @override
   Future<List<ProductImages>> getGallery() async {
     try {
-      Map<String, String> qParams = {'filter': 'product_id="78n4wqor3hhnkju"'};
+      Map<String, String> qParams = {'filter': 'product_id="at0y1gm0t65j62j"'};
       var response = await _dio.get(
         'collections/gallery/records',
         queryParameters: qParams,
@@ -42,4 +46,38 @@ class ProductDetailsRemoteDatasource extends IProductDetailsDataSource {
       throw ApiException(0, 'unknown error');
     }
   }
+
+  @override
+  Future<List<Variant>> getVariants() async {
+    try {
+      Map<String, String> qParams = {'filter': 'product_id="5vvww65pv6nviw6"'};
+      var response = await _dio.get(
+        'collections/variants/records',
+        queryParameters: qParams,
+      );
+      return response.data['items']
+          .map<Variant>((jsonObject) => Variant.fromMapJson(jsonObject))
+          .toList();
+    } on DioError catch (ex) {
+      throw ApiException(ex.response?.statusCode, ex.response?.data['message']);
+    } catch (ex) {
+      throw ApiException(0, 'unknown error');
+    }
+  }
+
+  @override
+  Future<List<ProductVariant>> getProductVariants() async {
+    var variantTypeList = await getVariantTypes();
+    var variantList = await getVariants();
+
+    List<ProductVariant> productVariantList = [];
+
+    for (var variantType in variantTypeList) {
+      var variantListArranged =
+          variantList.where((element) => element.typeId == variantType.id).toList();
+
+      productVariantList.add(ProductVariant(variantType, variantListArranged));
+    }
+    return productVariantList;
+  } 
 }
