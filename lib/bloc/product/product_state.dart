d71f6c4a37c1f1c83bbc92073fc486f339fb4da8
Alireza <alireza.kariminejad@yahoo.com>
2024-09-12 13:22:59 +0330

    add productCategory

--- lib/bloc/product/product_state.dart
+++ lib/bloc/product/product_state.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_shop_application/model/category.dart';
 import 'package:flutter_shop_application/model/product_images.dart';
 import 'package:flutter_shop_application/model/product_variant.dart';
 import 'package:flutter_shop_application/model/variant_type.dart';
@@ -12,6 +13,7 @@ class ProductDetailsInitialState extends ProductState {}
 class ProductDetailsResponseState extends ProductState {
   Either<String, List<ProductImages>> productImages;
   Either<String, List<ProductVariant>> productVariants;
-  ProductDetailsResponseState(this.productImages, this.productVariants);
+  Either<String, Category> productCategory;
+  ProductDetailsResponseState(this.productImages, this.productVariants, this.productCategory);
 }
  
