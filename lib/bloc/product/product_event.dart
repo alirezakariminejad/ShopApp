ce3048ddc0c8cf86b7e92736e4afd024ad2e5003
Alireza <alireza.kariminejad@yahoo.com>
2024-09-12 12:32:09 +0330

    init productId

--- lib/bloc/product/product_event.dart
+++ lib/bloc/product/product_event.dart
@@ -1,3 +1,6 @@
 abstract class ProductEvent {}
 
-class ProductInitalizeEvent extends ProductEvent {}
+class ProductInitalizeEvent extends ProductEvent {
+  String productId;
+  ProductInitalizeEvent(this.productId);
+}
