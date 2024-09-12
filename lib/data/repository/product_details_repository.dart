8956ca92db083fea33cd8c2e38718cd8e333f2e1
Alireza <alireza.kariminejad@yahoo.com>
2024-09-12 13:09:02 +0330

    add getProductCateogoryRepository

--- lib/data/repository/product_details_repository.dart
+++ lib/data/repository/product_details_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_shop_application/data/data_source/product_details_datasource.dart';
 import 'package:flutter_shop_application/di/di.dart';
+import 'package:flutter_shop_application/model/category.dart';
 import 'package:flutter_shop_application/model/product_images.dart';
 import 'package:flutter_shop_application/model/product_variant.dart';
 import 'package:flutter_shop_application/model/variant_type.dart';
@@ -10,6 +11,7 @@ abstract class IProductDetailsRepository {
   Future<Either<String, List<ProductImages>>> getProductImagesRepository(String productId);
   Future<Either<String, List<VariantType>>> getVariantTypeRepository();
   Future<Either<String, List<ProductVariant>>> getProductVariantsRepository();
+  Future<Either<String, Category>> getProductCateogoryRepository(String categoryId);
 }
 
 class ProductDetailsRepository extends IProductDetailsRepository {
@@ -39,6 +41,16 @@ class ProductDetailsRepository extends IProductDetailsRepository {
   Future<Either<String, List<ProductVariant>>> getProductVariantsRepository() async {
     try {
       var response = await _productDetailsDataSource.getProductVariants();
+      return right(response);
+    } on ApiException catch (ex) {
+      return left(ex.message ?? 'خطا محتوای متنی ندارد');
+    }
+  }
+
+  @override
+  Future<Either<String, Category>> getProductCateogoryRepository(String categoryId) async {
+    try {
+      var response = await _productDetailsDataSource.getProductCategory(categoryId);
       return right(response);
     } on ApiException catch (ex) {
       return left(ex.message ?? 'خطا محتوای متنی ندارد');
